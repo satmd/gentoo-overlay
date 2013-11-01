95749f6715cd05c039d3104ae89acc2c978729b7
satmd <satmd@satmd.dyndns.org>
2013-11-01 21:10:37 +0100

    fixed expression for allowing booting in restricted mode

--- sys-kernel/kernel-upgrader/files/90-grub-mkconfig.sh
+++ sys-kernel/kernel-upgrader/files/90-grub-mkconfig.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #exit 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
+sed -e 's,\(\s*\)echo,\1#echo,' -i /boot/grub2/grub.cfg
+sed -e 's,\(menuentry.*\)\(\s\),\1 --unrestricted\2,' -i /boot/grub2/grub.cfg
